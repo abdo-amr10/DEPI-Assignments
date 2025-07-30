f578888d0a079e70ecd23d619fd350da3edd3797
Abd El-Rahman Amr <amr838836@gmail.com>
2025-07-30 15:11:19 +0300

    Add files via upload

---
+++ CSharp -Task-Session-8 - SimpleCalculator/SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,56 @@
+﻿namespace SimpleCalculator
+{
+    internal class SimpleCalculator
+{
+        static double GetNum()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double num))
+                    return num;
+                else
+                    Console.WriteLine("Invalid number, please try again:");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello!");
+        
+        Console.WriteLine("Input the first number:");
+        double num1 = GetNum();
+
+        Console.WriteLine("Input the second number:");
+        double num2 = GetNum();
+
+        Console.WriteLine("What do you want to do with those numbers?");
+        Console.WriteLine("[A]dd");
+        Console.WriteLine("[S]ubtract");
+        Console.WriteLine("[M]ultiply");
+
+        string choice = Console.ReadLine().Trim().ToLower();
+
+        switch (choice)
+        {
+            case "a":
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                break;
+            case "s":
+                Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                break;
+            case "m":
+                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+
+        Console.WriteLine("Press any key to close");
+        Console.ReadKey();
+    }
+
+    
+    }
+}
