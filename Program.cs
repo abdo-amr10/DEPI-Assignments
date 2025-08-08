6d90a77ca5b0a455daeb1e945262fd6bd3204da1
Abd El-Rahman Amr <amr838836@gmail.com>
2025-08-08 20:25:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp__Task_Session_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank account1 = new Bank();
+
+            Bank account2 = new Bank("Sara Hassan","30005011234569","01234567890", 2500,"Alexandria, Egypt");
+
+            Bank account3 = new Bank( "Youssef Tarek", "30101011234560","01099887766", "Giza, Egypt");
+
+            account1.ShowAccountDetails();
+            account2.ShowAccountDetails();
+            account3.ShowAccountDetails();
+        }
+    }
+}
